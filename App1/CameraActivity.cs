5780ccf2711543968af50a1742e76dc1999c5443
Navid <Navid@LAPTOP-VV2V04J5>
2020-10-29 00:35:33 -0400

    Camera function implemented

--- App1/CameraActivity.cs
+++ App1/CameraActivity.cs
@@ -2,24 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Android;
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace App1
 {
     [Activity(Label = "CameraActivity")]
     public class CameraActivity : Activity
     {
+        Button captureButton;
+        ImageView imageView;
+
+        readonly string[] permissions =
+        {
+            Manifest.Permission.ReadExternalStorage,
+            Manifest.Permission.WriteExternalStorage,
+            Manifest.Permission.Camera
+        };
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_camera);
-            // Create your application here
+
+            captureButton = (Button)FindViewById(Resource.Id.capureButton);
+            imageView = (ImageView)FindViewById(Resource.Id.pictureView);
+
+            captureButton.Click += CaptureButton_Click;
+            RequestPermissions(permissions, 0);
+        }
+
+        async void TakePicture()
+        {
+            await CrossMedia.Current.Initialize();
+            MediaFile photo;
+            if (CrossMedia.Current.IsCameraAvailable)
+            {
+                photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions 
+                { 
+                    Directory = "MasterSaveReceipts",
+                    Name = "Receipt.jpg"
+                
+                });
+
+                if (photo == null)
+                {
+                    return;
+                }
+
+                byte[] bus = System.IO.File.ReadAllBytes(photo.Path);
+                Bitmap bitmap = BitmapFactory.DecodeByteArray(bus, 0, bus.Length);
+                imageView.SetImageBitmap(bitmap);
+            }
+            else
+            {
+                photo = await CrossMedia.Current.PickPhotoAsync();
+            }
+        }
+
+        private void CaptureButton_Click(object sender, EventArgs e)
+        {
+            TakePicture();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- App1/MainActivity.cs
+++ App1/MainActivity.cs
@@ -12,12 +12,16 @@ namespace App1
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        ImageButton cameraButton;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            cameraButton = (ImageButton)FindViewById(Resource.Id.cameraButton);
+            cameraButton.Click += OnCamera;
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -34,8 +38,7 @@ namespace App1
            vision.LoadImage();
            Toast.MakeText(Android.App.Application.Context, hello, ToastLength.Long).Show();
         }
-        [Java.Interop.Export("OnCamera")]
-        public void OnCamera(View view)
+        public void OnCamera(Object sender, EventArgs e)
         {
             var NxtAct = new Intent(this, typeof(CameraActivity));
             StartActivity(NxtAct);
